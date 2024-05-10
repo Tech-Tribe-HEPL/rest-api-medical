c41552e95badc2adf81ddd9055b8fa7f84b1a7c3
Andrea Spelgatti <spelgattiandrea@gmail.com>
2024-05-10 17:21:24 +0200

    Refactor + singleton db connect + cors

--- main.py
+++ main.py
@@ -2,46 +2,12 @@
 # Pressing F5 will run the app and open the browser with the swagger UI
 
 from apiflask import APIFlask
+from flask_cors import CORS
+import flask
 import psycopg
 import os
 
-app = APIFlask(__name__, title='Medical API', version='1.0')
-
-@app.get('/medical')
-def index():
-    # TODO: Check if this method is vulnerable to SQL Injection
-    user_uuid = "1234567890" # TODO: Extract from JWT token
-    conn.cursor().execute("""
-                        SELECT * FROM medicaltreatment 
-                        WHERE citizen = '%s';
-                        """, (user_uuid))
-    content = conn.cursor().fetchall()
-    flask.jsonify(content)
-    return flask.jsonify(content)
-
-@app.post('/medical')
-def post_medical():
-    conn.cursor().execute("""
-                        INSERT INTO medicaltreatment (MedicalTreatmentUniqueId, Description, Citizen, LeadDoctor)
-                        VALUES ('1','test','1234567890','1234567890');
-                        """)
-    conn.commit()
-    return '201 - Medical treatment created'
-
-@app.get('/medical/<int:id>')
-def get_medical(id):
-    #get the content from the database /!\ SQL Injection
-    id = flask.request.args.get('id')
-    conn.cursor().execute("""
-                        SELECT * FROM medicaltreatment
-                        WHERE id = %s AND citizen = '%s';
-                        """, (id, user_uuid))
-    #get the content from cursor in json format
-    content = conn.cursor().fetchall()
-    #parse the content into 
-    return flask.jsonify(content)
-
-if __name__ == '__main__':
+def get_db_connection():
     # Extract from envionnement variable information to connect to the database
     pg_user = os.getenv('POSTGRES_USER', 'postgres')
     pg_password = os.getenv('POSTGRES_PASSWORD', 'postgres')
@@ -52,5 +18,67 @@ if __name__ == '__main__':
     if pg_user == 'postgres' or pg_password == 'postgres' or pg_host == 'localhost' or pg_port == '5432' or pg_db == 'test':
         print('Warning: Using default values for database connection')
     # Connect to the database
-    with psycopg.connect("dbname=test user=postgres") as conn:
-        app.run(conn)
+    with psycopg.connect() as conn:
+        return conn
+
+def create_app(config_filename):
+    app = APIFlask(__name__, title='Medical API', version='1.0')
+    CORS(app)
+    @app.get('/medical')
+    def index():
+        conn = get_db_connection
+        # TODO: Check if this method is vulnerable to SQL Injection
+        user_uuid = "1234567890" # TODO: Extract from JWT token
+        
+        # Obtain get_db_connection
+        conn = get_db_connection
+        conn.cursor().execute("""
+                                SELECT * FROM medicaltreatment 
+                                WHERE citizen = '%s';
+                                """, (user_uuid))
+        content = conn.cursor().fetchall()
+        conn.close()
+        
+        flask.jsonify(content)
+        return flask.jsonify(content)
+
+    @app.post('/medical')
+    def post_medical():
+        conn = get_db_connection
+        conn.cursor().execute("""
+                                INSERT INTO medicaltreatment (MedicalTreatmentUniqueId, Description, Citizen, LeadDoctor)
+                                VALUES ('1','test','1234567890','1234567890');
+                                """)
+        conn.commit()
+        conn.close()
+        return '201 - Medical treatment created'
+
+    @app.get('/medical/<int:id>')
+    def get_medical(id):
+        #get the content from the database /!\ SQL Injection
+        id = flask.request.args.get('id')
+        # Obtain get_db_connection
+        conn = get_db_connection
+        
+        conn.cursor().execute("""
+                                SELECT * FROM medicaltreatment
+                                WHERE id = %s AND citizen = '%s';
+                                """, (id, user_uuid))
+        #get the content from cursor in json format
+        content = conn.cursor().fetchall()
+        conn.close()
+        #parse the content into
+        return flask.jsonify(content)
+
+    @app.errorhandler(404)
+    def not_found(error):
+        # This function will handle 404 errors
+        # display the full path in the response 
+        return flask.jsonify(flask.request.full_path)
+    return app
+
+
+app = create_app(__name__)
+
+if __name__ == "__main__":
+    app.run(debug=True)
